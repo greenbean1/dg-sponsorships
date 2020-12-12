7c7bd33ba5b0f871405defc4131563ac54bd102d
greenbean1 <beauhoover1@gmail.com>
2020-12-12 10:22:04 -0800

    email_functions working as hoped - need to clean up still

--- email_functions.py
+++ email_functions.py
@@ -1,15 +1,18 @@
-#Followed this tutorial: https://realpython.com/python-send-email/
-import smtplib, ssl
+# Sending emails via tutorial: https://realpython.com/python-send-email/
+import smtplib
+import ssl
+from email.mime.text import MIMEText
 import csv_functions
 from random import randrange
 
 EMAIL_PASSWORD_FILE = 'email_info.txt'
 EMAIL_SENDING_LOG = 'emailing_log.csv'
 
-RECEIVING_EMAILS = ['beauhoover1@gmail.com', 'uninathan@gmail.com', 'jancvanbruggen@gmail.com']
+# RECEIVING_EMAILS = ['beauhoover1@gmail.com', 'uninathan@gmail.com', 'jancvanbruggen@gmail.com']
+RECEIVING_EMAILS = 'beauhoover1@gmail.com'
+SENDER_EMAIL = "greenbeandev1@gmail.com"
 PORT = 465  # For SSL
 SMTP_SERVER = "smtp.gmail.com"
-SENDER_EMAIL = "greenbeandev1@gmail.com"
 
 
 def get_password():
@@ -17,30 +20,53 @@ def get_password():
     return file.read()
 
 
-def compose_message(names, article_url):
-    interesting_words = ['Interesting', 'Exciting', 'Shocking', 'Insane', 'Sweet', 'Mysterious']
-    interesting_word = interesting_words[randrange(len(interesting_words))]
-    message = """\
-    Subject: New Disc Golf Sponsorship Agreement
+def get_interesting_word():
+    interesting_words = ['Interesting', 'Exciting', 'Shocking', 'Insane', 'Sweet', 'Mysterious', 'Dank', 'Hot']
+    return interesting_words[randrange(len(interesting_words))]
 
-    X news! namesLIST were all sponsored!! Check article_url for more info"""
-    return message
+
+def compose_message(names, article_url):
+    interesting_word = get_interesting_word()
+    string_names = ' & '.join(names)
+    message = f"""\
+    Subject: New Disc Golf Sponsorship Agreement\n {interesting_word} news! {string_names} now sponsored! Check this article for more info: {article_url}"""
+    fixed_message = message.replace("\x0a", " ")
+    print(message)
+    return fixed_message
+
+
+def build_email_body(names, article_url):
+    interesting_word = get_interesting_word()
+    string_names = ' & '.join(names)
+    return f' {interesting_word} news! {string_names} now sponsored! Check this article for info: {article_url}'
+
+
+def build_full_email_message(names, article_url):
+    msg = MIMEText(build_email_body(names, article_url), 'html')
+    msg['Subject'] = 'New Disc Golf Sponsorship Agreement'
+    msg_string = msg.as_string()
+    return msg_string
+
+
+def test_simple_compose_message():
+    msg1 = MIMEText('BEAU HALLEAU', 'html')
+    msg1['Subject'] = 'YESSSSS'
+    msg2 = msg1.as_string()
+    return msg2
 
 
 def send_email(names, article_url):
-    for email_address in RECEIVING_EMAILS:
-        # Send Emails
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
+        for receiver_email in RECEIVING_EMAILS:
+            server.login(SENDER_EMAIL, get_password())
+            server.sendmail(SENDER_EMAIL, receiver_email, compose_message(names, article_url))
     csv_functions.log_timestamp(EMAIL_SENDING_LOG)
 
 
-receiver_email = "beauhoover1@gmail.com "  # Enter receiver address
-# password = input("Type your password and press enter: ")
-message = """\
-Subject: New Disc Golf Sponsorship Agreement
-
-This message was sent from Python. Pick up pswd from new place!"""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+def test_send_email(names, article_url):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
+        server.login(SENDER_EMAIL, get_password())
+        server.sendmail(SENDER_EMAIL, RECEIVING_EMAILS, build_full_email_message(names, article_url))
+    csv_functions.log_timestamp(EMAIL_SENDING_LOG)
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Process:
 # Modules to write
 import scrape_functions
 import csv_functions
-# import email_functions
+import email_functions
 
 DISC_GOLF_ARTICLE_URL = 'https://discgolf.ultiworld.com/2020/11/02/2021-player-sponsorship-tracker/'
 
@@ -20,6 +20,6 @@ dg_dict = scrape_functions.get_data(DISC_GOLF_ARTICLE_URL)
 if not csv_functions.csv_dg_info_exists():
     csv_functions.write_to_csv(dg_dict)
 incremental_names = csv_functions.get_incremental_names(dg_dict)
-# # If there is actually any incremental info, email me the info
-# if len(incremental_names) != 0:
-#     email_functions.send_email(incremental_names, DISC_GOLF_ARTICLE_URL)  # have this function log timestamp of emailing
+# if incremental_names:
+# email_functions.send_email(incremental_names, DISC_GOLF_ARTICLE_URL)
+email_functions.test_send_email(incremental_names,DISC_GOLF_ARTICLE_URL)
